8e420c52df1eef19b1f48bdfba5e11229b31f10e
mlgcode <mlgerbeth@outlook.com>
2022-08-17 00:03:30 +0200

    show what selected

--- src/main/java/com/myproject/DBClass.java
+++ src/main/java/com/myproject/DBClass.java
@@ -1,9 +1,12 @@
 package com.myproject;
 
+import com.mysql.cj.jdbc.result.ResultSetImpl;
+
 import javax.swing.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.*;
+import java.util.Collections;
 
 public class DBClass {
     String conString = "jdbc:mysql://localhost:3306/codelibary";
@@ -18,18 +21,23 @@ public class DBClass {
         if(search.equals("")){
             sql = "SELECT title from codelibary";
         } else {
-            sql = "SELECT title from codelibary WHERE title like %" +search+ "% OR text like %" +search+ "%";
+            sql = "SELECT title from codelibary WHERE title LIKE '%" +search+ "%' OR text LIKE '%" +search+ "%'";
         }
 
         try{
             // load and register JDBC driver for MySQ
             Connection con = DriverManager.getConnection(conString, username, password);
             Statement s = con.prepareStatement(sql);
-            ResultSet rs = s.executeQuery(sql);
+            ResultSet rs = null;
 
-            while(rs.next()){
-                String name = rs.getString(1);
-                dm.addElement(name);
+            try {
+                rs = s.executeQuery(sql);
+                while(rs.next()){
+                    String name = rs.getString(1);
+                    dm.addElement(name);
+                }
+            } catch(Exception ex) {
+                return dm;
             }
             return dm;
 
@@ -38,7 +46,24 @@ public class DBClass {
         }
 
         return null;
+    }
 
+    public String getText(String title){
+        String sql = "SELECT text from codelibary WHERE title = '" + title + "'";
+        String text = "";
 
+        try {
+            // load and register JDBC driver for MySQ
+            Connection con = DriverManager.getConnection(conString, username, password);
+            Statement s = con.prepareStatement(sql);
+            ResultSet rs = s.executeQuery(sql);
+            if(rs.next()) {
+                text = (String) rs.getString(1);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return text;
     }
 }
