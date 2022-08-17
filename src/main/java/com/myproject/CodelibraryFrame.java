70c55146d626f9c7cb5676e295b85bd2c0faea5c
mlgcode <michelgerbeth@outlook.de>
2022-08-17 10:36:34 +0200

    update develop

--- src/main/java/com/myproject/CodelibraryFrame.java
+++ src/main/java/com/myproject/CodelibraryFrame.java
@@ -1,6 +1,8 @@
 package com.myproject;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,6 +13,7 @@ public class CodelibraryFrame extends JFrame {
     private JTextPane textPane1;
     private JButton searchButton;
     private JList list1;
+
     public CodelibraryFrame(String title) {
         super(title);
 
@@ -20,8 +23,22 @@ public class CodelibraryFrame extends JFrame {
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-               DefaultListModel dm = new DBClass().searchdata(sadFormattedTextField.getText());
-               list1.setModel(dm);
+                DefaultListModel dm = new DBClass().searchdata(sadFormattedTextField.getText());
+                list1.clearSelection();
+                list1.setModel(dm);
+            }
+        });
+        list1.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                String title = "";
+                try {
+                    title = list1.getSelectedValue().toString();
+                } catch (Exception ex1) {
+                    return;
+                }
+                textPane1.setText(title);
+                textArea1.setText(new DBClass().getText(title));
             }
         });
     }
